55390c453ed4e4fa1fa844977c13f898a163b577
Shlok dash <142171908+Shldap@users.noreply.github.com>
2023-12-09 20:59:26 +0000

    Create chord_progressions.py

---
+++ MELODYGENERATOR2/chord_progressions.py
@@ -0,0 +1,15 @@
+
+# Define the library of commonly used chord progressions
+chord_progressions = {
+    "I-IV-V": [
+        ["C", "F", "G"],  # Chord progression in the key of C major
+        ["D", "G", "A"],  # Chord progression in the key of D major
+        ["G", "C", "D"]   # Chord progression in the key of G major
+    ],
+    "vi-IV-I-V": [
+        ["Am", "F", "C", "G"],  # Chord progression in the key of C major
+        ["Bm", "G", "D", "A"],  # Chord progression in the key of D major
+        ["Em", "C", "G", "D"]   # Chord progression in the key of G major
+    ],
+    # Add more chord progressions as needed
+}
